1efa7c95ac3489f9c2bb2ba15b686f95c88299a9
Arquetipo28 <german.reynagaa@gmail.com>
2020-10-09 22:19:49 -0500

    fix(index): using the correct indentation
    Usually indentation makes it difficult to read the code

--- index.js
+++ index.js
@@ -5,29 +5,28 @@ const defaultOptions = {
     examplePath: process.cwd()
 }
 module.exports = {
-    config: function (options = {}) {
-
-        const configOptions = { ...defaultOptions, ...options }
-        const parsedEnv = config(configOptions).parsed
-        const quasarEnv = {}
-        for (const key in parsedEnv) {
-            if (parsedEnv.hasOwnProperty(key)) {
-                quasarEnv[key] = JSON.stringify(parsedEnv[key])
-            }
-        }
-
-        const fs = require('fs')
-        const path = require('path')
-        const encoding = configOptions.encoding
-        const baseEnvPath = path.resolve(configOptions.examplePath, '.env.example')
-        const baseEnvBuffer = fs.readFileSync(baseEnvPath, { encoding })
-        const baseEnv = parse(baseEnvBuffer)
-        for (const key in baseEnv) {
-            if (baseEnv.hasOwnProperty(key)) {
-                quasarEnv[key] = JSON.stringify(process.env[key])
-            }
-        }
-
-        return quasarEnv
+  config: function (options = {}) {
+    const configOptions = { ...defaultOptions, ...options }
+    const parsedEnv = config(configOptions).parsed
+    const quasarEnv = {}
+    for (const key in parsedEnv) {
+      if (parsedEnv.hasOwnProperty(key)) {
+        quasarEnv[key] = JSON.stringify(parsedEnv[key])
+      }
     }
+
+    const fs = require('fs')
+    const path = require('path')
+    const encoding = configOptions.encoding
+    const baseEnvPath = path.resolve(configOptions.examplePath, '.env.example')
+    const baseEnvBuffer = fs.readFileSync(baseEnvPath, { encoding })
+    const baseEnv = parse(baseEnvBuffer)
+    for (const key in baseEnv) {
+      if (baseEnv.hasOwnProperty(key)) {
+        quasarEnv[key] = JSON.stringify(process.env[key])
+      }
+    }
+
+    return quasarEnv
+  }
 }
